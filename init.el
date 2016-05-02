097f40d0c1b9a9f02e7acc34d77001754d50a74e
jon <jon.programming@gmail.com>
2016-05-02 14:50:43 -0400

    added more airline stuff

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(linum ((t (:background "dim gray" :foreground "gainsboro")))))
 
 ;;setup backup saves
 (setq backup-directory-alist `(("." . "~/.saves")))
