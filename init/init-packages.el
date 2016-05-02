f3f1f0f73f9aa5f6c505adf2db30788a6fd1e56c
jon <jon.programming@gmail.com>
2016-05-02 14:04:44 -0400

    added marmalade

--- init/init-packages.el
+++ init/init-packages.el
@@ -2,6 +2,7 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'load-path "~/.emacs.d/airline-themes/")
 (setq package-enable-at-startup nil)
 
